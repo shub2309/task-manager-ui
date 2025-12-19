ea6c8f2b97defa6e7a0fb90a670f3d9fc3000279
Shubham Jadhav <shubhamnj23@gmail.com>
2025-12-19 14:20:47 +0530

    changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
     'Accept': 'application/json',
   },
 });
